05fd5cd5673add657d7204dd531775383bf400bb
Nick Stielau <nick.stielau@gmail.com>
2010-10-07 13:37:29 -0700

    Adding embed filters.

--- limelight.php
+++ limelight.php
@@ -22,4 +22,51 @@ function ll_video_options() {
 
   include('ll_admin.php');
 }
+
+
+
+// Embed Code Filter
+
+// "[limelight mediaId=MEDIA_ID]"
+// "[limelight channelId=CHANNEL_ID]"
+// "[limelight channelId=CHANNEL_ID 210 175]"
+// "[limelight channelId=CHANNEL_ID&someFlashVar=SOME_FLASHVAR 210 175]"
+
+define("LIMELIGHT_WIDTH", 480); // default width
+define("LIMELIGHT_HEIGHT", 411); // default height
+define("LIMELIGHT_REGEXP", "/\[limelight ([[:print:]]+)\]/");
+define("LIMELIGHT_TARGET",'<object width="###WIDTH###" height="###HEIGHT###" id="delve_player440704o" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"><param name="movie" value="http://assets.delvenetworks.com/player/loader.swf"/><param name="wmode" value="window"/><param name="allowScriptAccess" value="always"/><param name="allowFullScreen" value="true"/><param name="flashvars" value="###FLASHVARS####"/><embed src="http://assets.delvenetworks.com/player/loader.swf" name="delve_player440704e" wmode="window" width="###WIDTH###" height="###HEIGHT###" allowScriptAccess="always" allowFullScreen="true" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" flashvars="###FLASHVARS####"></embed></object>');
+
+function limelight_plugin_callback($match) {
+  $tag_parts = explode(" ", rtrim($match[0], "]"));
+  $flashvars = $tag_parts[1];
+  $width = $tag_parts[2];
+  $height = $tag_parts[3];
+  $output = LIMELIGHT_TARGET;
+  $output = str_replace("###FLASHVARS###", $tag_parts[1], $output);
+  if (count($tag_parts) > 2) {
+    if ($tag_parts[2] == 0) {
+      $output = str_replace("###WIDTH###", LIMELIGHT_WIDTH, $output);
+    } else {
+      $output = str_replace("###WIDTH###", $tag_parts[2], $output);
+    }
+    if ($tag_parts[3] == 0) {
+      $output = str_replace("###HEIGHT###", LIMELIGHT_HEIGHT, $output);
+    } else {
+      $output = str_replace("###HEIGHT###", $tag_parts[3], $output);
+    }
+  } else {
+    $output = str_replace("###WIDTH###", LIMELIGHT_WIDTH, $output);
+    $output = str_replace("###HEIGHT###", LIMELIGHT_HEIGHT, $output);
+  }
+  return ($output);
+}
+
+function limelight_plugin($content) {
+  return (preg_replace_callback(LIMELIGHT_REGEXP, 'limelight_plugin_callback', $content));
+}
+
+add_filter('the_content', 'limelight_plugin',1);
+add_filter('the_content_rss', 'limelight_plugin');
+add_filter('the_excerpt', 'limelight_plugin');
 ?>
