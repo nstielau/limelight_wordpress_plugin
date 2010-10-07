f744a0df2c7e66b5af14c6339948cd55025256af
Nick Stielau <nick.stielau@gmail.com>
2010-10-07 14:24:49 -0700

    Whitespace.

--- ll_admin.php
+++ ll_admin.php
@@ -2,30 +2,30 @@
 /**
  * The HTML for the admin options page.
  *
- * @package WordPress
  */
-  if($_POST['limelight_hidden'] == 'Y') {
-    //Form data sent
-    $ll_org_id = $_POST['ll_org_id'];
-    update_option('ll_org_id', $ll_org_id);
 
-    $ll_default_width = $_POST['ll_default_width'];
-    update_option('ll_default_width', $ll_default_width);
+if($_POST['limelight_hidden'] == 'Y') {
+  //Form data sent
+  $ll_org_id = $_POST['ll_org_id'];
+  update_option('ll_org_id', $ll_org_id);
 
-    $ll_default_height = $_POST['ll_default_height'];
-    update_option('ll_default_height', $ll_default_height);
+  $ll_default_width = $_POST['ll_default_width'];
+  update_option('ll_default_width', $ll_default_width);
 
-    $ll_additional_flashvars = $_POST['ll_additional_flashvars'];
-    update_option('ll_additional_flashvars', $ll_additional_flashvars);
-    ?>
-    <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
-    <?php
-  } else {
-    //Normal page display
-    $ll_org_id = get_option('ll_org_id');
-    $ll_default_width = get_option('ll_default_width');
-    $ll_default_height = get_option('ll_default_height');
-  }
+  $ll_default_height = $_POST['ll_default_height'];
+  update_option('ll_default_height', $ll_default_height);
+
+  $ll_additional_flashvars = $_POST['ll_additional_flashvars'];
+  update_option('ll_additional_flashvars', $ll_additional_flashvars);
+  ?>
+  <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+  <?php
+} else {
+  //Normal page display
+  $ll_org_id = get_option('ll_org_id');
+  $ll_default_width = get_option('ll_default_width');
+  $ll_default_height = get_option('ll_default_height');
+}
 ?>
 
 <div class="wrap">
@@ -37,7 +37,7 @@
     <p><?php _e("Video player width (px): " ); ?><input type="text" name="ll_default_width" value="<?php echo $ll_default_width; ?>" size="5"><?php _e(" ex: 480" ); ?></p>
     <p><?php _e("Video player height (px): " ); ?><input type="text" name="ll_default_height" value="<?php echo $ll_default_height; ?>" size="5"><?php _e(" ex: 411" ); ?></p>
     <p><em>Warning</em>: This will change the height and width for any videos that don&quot;t have specific dimensions set. </p>
-    <p><?php _e("Additional Flashvars: " ); ?><input type="text" name="ll_additional_flashvars" value="<?php echo $ll_additional_flashvars; ?>" size="5"><?php _e(" ex: deepLink=true" ); ?></p>
+    <p><?php _e("Additional Flashvars: " ); ?><input type="text" name="ll_additional_flashvars" value="<?php echo $ll_additional_flashvars; ?>" size="40"><?php _e(" ex: deepLink=true" ); ?></p>
     <p>These Flashvars will be added before any specific Flashvars</p>
 
     <?php    echo "<h4>" . __( 'Limelight API Settings', 'limelight_text_domain' ) . "</h4>"; ?>
