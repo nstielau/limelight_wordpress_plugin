2597647a79f85af1960b68ac981efabb65626cb5
Nick Stielau <nick.stielau@gmail.com>
2010-10-08 13:21:27 -0700

    Adding stuff.

--- ll_tinymce.php
+++ ll_tinymce.php
@@ -15,18 +15,18 @@ add_action('init', 'add_ll_tinymce_buttons');
 function add_ll_tinymce_buttons() {
 	if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) return;
 	if ( get_user_option('rich_editing') == 'true') {
-		add_filter("mce_external_plugins", "add_tinymce_plugin", 5);
-		add_filter('mce_buttons', 'register_button', 5);
+		add_filter("mce_external_plugins", "ll_add_tinymce_plugin", 5);
+		add_filter('mce_buttons', 'll_register_button', 5);
 	}
 }
 
-function register_button($buttons) {
+function ll_register_button($buttons) {
 	array_push($buttons, "separator", $ll_tinymce_pluginname );
 	return $buttons;
 }
 
-function add_tinymce_plugin($plugin_array) {
-	$plugin_array[$ll_tinymce_pluginname] =  "http://foo.com/jah.js";
+function ll_add_tinymce_plugin($plugin_array) {
+	$plugin_array[$ll_tinymce_pluginname] =  "http://ec2-174-129-76-127.compute-1.amazonaws.com/wordpress/wp-content/limelight/tinymce.js";
 	return $plugin_array;
 }
 
---
+++ tinymce.js
@@ -0,0 +1,64 @@
+// Docu : http://wiki.moxiecode.com/index.php/TinyMCE:Create_plugin/3.x#Creating_your_own_plugins
+
+(function() {
+	// Load plugin specific language pack
+	tinymce.PluginManager.requireLangPack('wordTube');
+
+	tinymce.create('tinymce.plugins.ll', {
+		/**
+		 * Initializes the plugin, this will be executed after the plugin has been created.
+		 * This call is done before the editor instance has finished it's initialization so use the onInit event
+		 * of the editor instance to intercept that event.
+		 *
+		 * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
+		 * @param {string} url Absolute URL to where the plugin is located.
+		 */
+		init : function(ed, url) {
+			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceExample');
+
+			ed.addCommand('mce_ll', function() {
+				ed.windowManager.open({
+					file : url + '/window.php',
+					width : 360 + ed.getLang('wordTube.delta_width', 0),
+					height : 210 + ed.getLang('wordTube.delta_height', 0),
+					inline : 1
+				}, {
+					plugin_url : url // Plugin absolute URL
+				});
+			});
+
+			// Register example button
+			ed.addButton('ll', {
+				title : 'Somethign about limelight',
+				cmd : 'mce_ll',
+				image : url + '/wordtube.gif'
+			});
+
+			// Add a node change handler, selects the button in the UI when a image is selected
+			ed.onNodeChange.add(function(ed, cm, n) {
+				cm.setActive('wordTube', n.nodeName == 'IMG');
+			});
+		},
+
+		/**
+		 * Returns information about the plugin as a name/value array.
+		 * The current keys are longname, author, authorurl, infourl and version.
+		 *
+		 * @return {Object} Name/value array containing information about the plugin.
+		 */
+		getInfo : function() {
+			return {
+					longname  : 'wordTube',
+					author 	  : 'Alex Rabe',
+					authorurl : 'http://alexrabe.boelinger.com',
+					infourl   : 'http://alexrabe.boelinger.com',
+					version   : "2.0"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('wordTube', tinymce.plugins.wordTube);
+})();
+
+
