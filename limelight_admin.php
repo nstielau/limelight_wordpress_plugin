2273688dc8df5e343b7f37a33f8093f14732463d
Nick Stielau <nick.stielau@gmail.com>
2010-10-15 14:32:21 -0700

    Adding docs.

--- limelight_admin.php
+++ limelight_admin.php
@@ -48,14 +48,14 @@ if($_POST['limelight_hidden'] == 'Y') {
   <form name="limelight_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
     <input type="hidden" name="limelight_hidden" value="Y">
 
-    <?php    echo "<h4>" . __( 'Limelight Embed Code Settings', 'limelight_text_domain' ) . "</h4>"; ?>
+    <?php    echo "<h4>" . __( 'Limelight Networks Embed Code Settings', 'limelight_text_domain' ) . "</h4>"; ?>
     <p><?php _e("Video player width (px): " ); ?><input type="text" name="ll_default_width" value="<?php echo $ll_default_width; ?>" size="5"><?php _e(" ex: 480" ); ?></p>
     <p><?php _e("Video player height (px): " ); ?><input type="text" name="ll_default_height" value="<?php echo $ll_default_height; ?>" size="5"><?php _e(" ex: 411" ); ?></p>
     <p><em>Warning</em>: This will change the height and width for any players that do not have specific dimensions set. </p>
     <p><?php _e("Additional Flashvars: " ); ?><input type="text" name="ll_additional_flashvars" value="<?php echo $ll_additional_flashvars; ?>" size="40"><?php _e(" ex: deepLink=true&var=val" ); ?></p>
     <p>These Flashvars will be added before any specific Flashvars</p>
 
-    <?php    echo "<h4>" . __( 'Limelight API Settings', 'limelight_text_domain' ) . "</h4>"; ?>
+    <?php    echo "<h4>" . __( 'Limelight Networks API Settings', 'limelight_text_domain' ) . "</h4>"; ?>
     <p><?php _e("Organization ID: " ); ?><input type="text" name="ll_org_id" value="<?php echo $ll_org_id; ?>" size="45"><?php _e(" ex: 1fcedd0a66334ac28fbb2a4117707145" ); ?></p>
 
     <p class="submit">
