0e6f1a9df734b28d70f91f339d8f96bdfcf0fc1d
Nick Stielau <nick.stielau@gmail.com>
2010-10-15 12:48:49 -0700

    Consolidate plugins.

--- limelight.php
+++ limelight.php
@@ -78,4 +78,59 @@ function limelight_plugin($content) {
 add_filter('the_content', 'limelight_plugin',1);
 add_filter('the_content_rss', 'limelight_plugin');
 add_filter('the_excerpt', 'limelight_plugin');
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Adding the actions and hooks for admin menu, and embed-code short code replacement.
+//////////////////////////////////////////////////////////////////////////////////////
+
+class add_ll_button {
+
+  var $pluginname = "limelight";
+
+  function add_ll_button()  {
+    // Modify the version when tinyMCE plugins are changed.
+    add_filter('tiny_mce_version', array (&$this, 'change_tinymce_version') );
+
+    // init process for button control
+    add_action('init', array (&$this, 'addbuttons') );
+  }
+
+  function addbuttons() {
+    // Don't bother doing this stuff if the current user lacks permissions
+    if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) return;
+
+    // Add only in Rich Editor mode
+    if ( get_user_option('rich_editing') == 'true') {
+      // add the button for wp2.5 in a new way
+      add_filter("mce_external_plugins", array (&$this, "add_tinymce_plugin" ), 5);
+      add_filter('mce_buttons', array (&$this, 'register_button' ), 5);
+    }
+  }
+
+  // used to insert button in wordpress 2.5x editor
+  function register_button($buttons) {
+
+    array_push($buttons, "separator", $this->pluginname );
+
+    return $buttons;
+  }
+
+  // Load the TinyMCE plugin : editor_plugin.js (wp2.5)
+  function add_tinymce_plugin($plugin_array) {
+    $site_url = get_option('siteurl');
+    $plugin_array[$this->pluginname] =  "$site_url/wp-content/plugins/limelight/editor_plugin.js";
+
+    return $plugin_array;
+  }
+
+  function change_tinymce_version($version) {
+    return ++$version;
+  }
+}
+
+// Call it now
+$ll_tinymce_button = new add_ll_button();
 ?>
