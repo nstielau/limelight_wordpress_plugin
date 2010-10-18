315f78c4eedc354b50f0cc309afe8b282f312258
Nick Stielau <nick.stielau@gmail.com>
2010-10-18 15:14:44 -0700

    Sanitizing I18N stuff.

--- limelight_admin.php
+++ limelight_admin.php
@@ -22,7 +22,7 @@ if($_POST['limelight_hidden'] == 'Y') {
   update_option('limelight_media_cache_file', "");
   update_option('limelight_channels_cache_file', "");
   ?>
-  <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+  <div class="updated"><p><strong><?php echo __('Options saved.', 'limelight_text_domain'); ?></strong></p></div>
   <?php
 } else {
   //Normal page display
@@ -34,9 +34,9 @@ if($_POST['limelight_hidden'] == 'Y') {
 ?>
 
 <div class="wrap">
-  <?php    echo "<h2>" . __( 'Limelight Networks Options', 'limelight_text_domain' ) . "</h2>"; ?>
+  <?php echo "<h2>" . __( 'Limelight Networks Options', 'limelight_text_domain' ) . "</h2>"; ?>
 
-  <?php    echo "<h4>" . __( 'Limelight Networks Embed Codes', 'limelight_text_domain' ) . "</h4>"; ?>
+  <?php echo "<h4>" . __( 'Limelight Networks Embed Codes', 'limelight_text_domain' ) . "</h4>"; ?>
   <p>
     To insert a video or channel into a post or page, using the following shortcode:<code>[limelight FLASHVARS WIDTH HEIGHT]</code>where FLASHVARS is a <a href="http://kb2.adobe.com/cps/164/tn_16417.html">string of variables</a> to pass to the player, and HEIGHT and WIDTH are optional dimensions in pixels.  This shortcode gets transformed into an embedcode when the post/page is loaded.
   </p>
@@ -52,14 +52,14 @@ if($_POST['limelight_hidden'] == 'Y') {
     <input type="hidden" name="limelight_hidden" value="Y">
 
     <?php    echo "<h4>" . __( 'Limelight Networks Embed Code Settings', 'limelight_text_domain' ) . "</h4>"; ?>
-    <p><?php _e("Video player width (px): " ); ?><input type="text" name="ll_default_width" value="<?php echo $ll_default_width; ?>" size="5"><?php _e(" ex: 480" ); ?></p>
-    <p><?php _e("Video player height (px): " ); ?><input type="text" name="ll_default_height" value="<?php echo $ll_default_height; ?>" size="5"><?php _e(" ex: 411" ); ?></p>
+    <p><?php _e("Video player width (px): ", 'limelight_text_domain'); ?><input type="text" name="ll_default_width" value="<?php echo $ll_default_width; ?>" size="5"><?php _e(" ex: 480" ); ?></p>
+    <p><?php _e("Video player height (px): ", 'limelight_text_domain'); ?><input type="text" name="ll_default_height" value="<?php echo $ll_default_height; ?>" size="5"><?php _e(" ex: 411" ); ?></p>
     <p><em>Warning</em>: Changing these dimension settings with update the height and width for any players that do not have specific dimensions set. </p>
-    <p><?php _e("Additional Flashvars: " ); ?><input type="text" name="ll_additional_flashvars" value="<?php echo $ll_additional_flashvars; ?>" size="40"><?php _e(" ex: deepLink=true&var=val" ); ?></p>
+    <p><?php _e("Additional Flashvars: ", 'limelight_text_domain'); ?><input type="text" name="ll_additional_flashvars" value="<?php echo $ll_additional_flashvars; ?>" size="40"><?php _e(" ex: deepLink=true&var=val" ); ?></p>
     <p>These Flashvars will be added before any specific Flashvars</p>
 
-    <?php echo "<h4>" . __( 'Limelight Networks API Settings', 'limelight_text_domain' ) . "</h4>"; ?>
-    <p><?php _e("Organization ID: " ); ?><input type="text" name="ll_org_id" value="<?php echo $ll_org_id; ?>" size="45"><?php _e(" ex: 1fcedd0a66334ac28fbb2a4117707145" ); ?></p>
+    <?php echo "<h4>" . __( 'Limelight Networks API Settings', 'limelight_text_domain') . "</h4>"; ?>
+    <p><?php _e("Organization ID: " ); ?><input type="text" name="ll_org_id" value="<?php echo $ll_org_id; ?>" size="45"><?php echo __(" ex: 1fcedd0a66334ac28fbb2a4117707145", 'limelight_text_domain'); ?></p>
 
     <p class="submit">
     <input type="submit" name="Submit" value="<?php _e('Update Options', 'limelight_text_domain' ) ?>" />
